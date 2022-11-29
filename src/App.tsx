729d8ea13a5f48d6fb8f9b17758026dc0d2c8934
shailesh74250 <smourya7425@gmail.com>
2022-11-29 15:36:38 +0530

    Router done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./components/Home";
+import { Store } from "./components/Store";
+import { About } from "./components/About";
+
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   );
 }
 
